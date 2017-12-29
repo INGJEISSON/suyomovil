476756d2e8869e01869ca62101efb59e470a8879
jeisson <jibarguen@suyo.co>
2017-12-29 12:02:50 -0500

    Iniciando en suyo movil

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+/** 
+* Database config variables 
+*/ 
+define("DB_HOST", "localhost"); 
+define("DB_USER", "root"); 
+define("DB_PASSWORD", ""); 
+define("DB_DATABASE", "plataformasuyo"); 
+/* 
+* Google API Key 
+*/ 
+define("GOOGLE_API_KEY", "AIzaSyBHwjNbfj3NJB5WmhAAXAkjYKDdWU8KZvo"); 
+// Coloca tu Api key aquí 
+?>
